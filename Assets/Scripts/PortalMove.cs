2f344764bc897a4d5808f278f563603a32e88411
Mark Timcenko <mt3523@nyu.edu>
2020-04-02 12:31:39 -0400

    Early Portal Mechanics
    Buggy portal mechanics, not done yet.  Testing if I can push.

---
+++ Assets/Scripts/PortalMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalMove : MonoBehaviour
+{
+    private float forceVar = 500;
+
+    Rigidbody RB;
+
+    //  AudioSource AS;
+    // public AudioClip ball;
+    // public AudioClip strike;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // AS = GetComponent<AudioSource>();
+        RB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+ 
+
+    private void FixedUpdate()
+    {
+        RB.AddForce(transform.forward * forceVar);
+    }
+
+   
+}
