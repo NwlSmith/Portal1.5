69f2416490bfb9099e36521d1aae367642482e92
IsabellaChen <emaxx2000@163.com>
2020-04-02 14:09:08 -0500

    Add picking up objects scripts

---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+* Date created: 4/2/2020
+* Creator: Isabella Chen
+* 
+* Description: Picking up objects script.
+* Please attach this script to every object that player needs to pick up.
+*/
+public class Pickupable : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
