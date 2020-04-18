7098600d1d5b5c1bad9a7e93f26c471c9c2247dd
kyouma589 <rlothian589@gmail.com>
2020-04-18 16:56:28 -0400

    Player Model is now rigged! script for MEG also added but not fully implemented yet

--- Assets/Scripts/MaterialEmancipationGrill.cs
+++ Assets/Scripts/MaterialEmancipationGrill.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*
+ * Date Created: 4/18/2020
+ * Creator: Raymond Lothian
+ * 
+ * Description: Destroys any objects other than the player and closes all portals when passing through.
+ */
 
 public class MaterialEmancipationGrill : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+
+
+    // Start is called before the first frame update
+    
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    //Checks if the object passing through the 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Grill is working");
+            
+
+        }
+       
+
+    }
+
 }
