f64b56f5187bb936604e58fe75bd6c21d3c35041
Nate Smith <me@nwlsmith.com>
2020-04-25 23:40:43 -0500

    Implemented portal gun onto model and added firing animations. Fixed bug where going through MEG didn't reset reticle.

---
+++ Assets/Scripts/FollowRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowRotation : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 10f;
+    public Quaternion offset;
+    private Quaternion lastRotation;
+
+    private void Start()
+    {
+        offset = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = lastRotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, speed * Time.deltaTime);
+        lastRotation = transform.rotation;
+    }
+}
